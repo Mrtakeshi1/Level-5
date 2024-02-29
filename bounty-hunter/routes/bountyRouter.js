67b59f18eb87897eb7a2b5ce04802ed565ac3b91
Takeshi Garcia <garcia.takeshi@yahoo.com>
2024-02-28 19:13:04 -0800

    Update

--- bounty-hunter/routes/bountyRouter.js
+++ bounty-hunter/routes/bountyRouter.js
@@ -9,24 +9,47 @@ const bounties = [
     { name: "Qui-Gon Jinn", living: true, bounty: 3000, type: "Jedi", _id: uuid() }
 ];
 
+
 // GET all bounties
 bountyRouter.get("/", (req, res) => {
-    res.send(bounties);
+    res.send(bounties)
 });
 
-// GET a specific bounty by ID
+
+// GET a specific bounty by ID //
 bountyRouter.get("/:bountyId", (req, res) => {
-    const bountyId = req.params.bountyId;
+    const bountyId = req.params.bountyId
     const foundBounty = bounties.find(bounty => bounty._id === bountyId)
-    res.send(foundBounty);
-}); 
+    res.send(foundBounty)
+});
+
 
 // POST a new bounty
 bountyRouter.post("/", (req, res) => {
-    const newBounty = req.body;
-    newBounty._id = uuid();
-    bounties.push(newBounty);
+    const newBounty = req.body
+    newBounty._id = uuid()
+    bounties.push(newBounty)
     res.send(`New Bounty added ${newBounty.name} to the database!`);
 });
+
+
+// DELETE a bounty by ID
+bountyRouter.delete("/:bountyId", (req, res) => {
+    const bountyId = req.params.bountyId
+    const bountyIndex = bounties.findIndex(bounty => bounty._id === bountyId)
+    bounties.splice(bountyIndex, 1)
+    res.send("Bounty deleted successfully")
+})
+
+
+// PUT (update) an existing bounty by ID
+bountyRouter.put("/:bountyId", (req, res) => {
+    const bountyId = req.params.bountyId
+    const bountyIndex = bounties.findIndex(bounty => bounty._id === bountyId)
+    const updatedBounty = bountyIndex !== -1 ? Object.assign(bounties[bountyIndex], req.body) : null
+    res.send(updatedBounty)
+});
+
+
 
 module.exports = bountyRouter;
